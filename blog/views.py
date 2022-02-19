be2d5192785ceac11368c0bfbde56a301c9d9e7a
pevescarol <carol.peves@gmail.com>
2022-02-19 12:23:21 -0300

    add  changes

---
+++ blog/views.py
@@ -0,0 +1,10 @@
+import re
+from django.shortcuts import get_object_or_404 ,render, redirect
+from django.views.generic import View
+
+# Create your views here.
+class BlogListView(View):
+    def get(self, request, *args, **kwargs):
+        context = {
+        }
+        return render(request,'blog_list.html', context)
