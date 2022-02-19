3e7f82e794b06874116ef07756a12c8e4f89376a
pevescarol <carol.peves@gmail.com>
2022-02-19 19:45:29 -0300

    add form test

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from blog.views import BlogListView
+from blog.views import BlogListView, BlogCreateView
 
 
 app_name="blog"
 
 urlpatterns = [
-    path('',BlogListView.as_view(), name="home")
+    path('',BlogListView.as_view(), name="home"),
+    path('create/',BlogCreateView.as_view(), name="create")
 ]
